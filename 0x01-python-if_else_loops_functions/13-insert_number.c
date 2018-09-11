e11913843da281d069c47fcd182fda5b2e03ba47
JackWanaCode <kazucuong@yahoo.com>
2018-09-11 22:20:31 +0000

    correct case add on begin of list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -11,7 +11,7 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *prev = *head;
+	listint_t *prev = NULL;
 	listint_t *curr = *head;
 	listint_t *new = NULL;
 
@@ -26,11 +26,9 @@ listint_t *insert_node(listint_t **head, int number)
 	}
 	while (curr)
 	{
-		prev = curr;
-		curr = curr->next;
 		if (curr && curr->n > number)
 		{
-			if (prev == *head)
+			if (prev == NULL)
 				*head = new;
 			else
 				prev->next = new;
@@ -39,6 +37,8 @@ listint_t *insert_node(listint_t **head, int number)
 		}
 		else if (curr->n == number)
 			return (NULL);
+		prev = curr;
+                curr = curr->next;
 	}
 	if (prev && prev->n < number)
 	{
