37a481828f8de6d4b18421a55bcfc4aeff8b378f
JackWanaCode <kazucuong@yahoo.com>
2018-09-13 19:31:44 +0000

    use len(argv)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    i = 0; j = 0
-    for ar in sys.argv:
-        i += 1
-    if i == 1:
+    from sys import argv
+    j = 0
+    if len(argv) == 1:
         print("0 arguments.")
     else:
-        for ar in sys.argv:
+        for ar in argv:
             if j == 0:
-                print("{} argument:".format(i - 1))
+                print("{} argument:".format(len(argv) - 1))
             else:
                 print("{}: {}".format(j, ar))
             j += 1
