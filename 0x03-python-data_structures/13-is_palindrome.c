c3b4410c0239b4ef83022c4f1b08b25191ee1b81
JackWanaCode <kazucuong@yahoo.com>
2018-09-18 21:05:14 +0000

    change method - store number in an array

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,28 +3,44 @@
 #include "lists.h"
 
 /**
+ * check_pal - check is palindrome array
+ * @arr: pointer to array
+ * @len: length of array
+ * Return: 1 is palindrome, else 0
+ */
+int check_pal(char *arr, int len)
+{
+	int i, j;
+
+	for (i = 0, j = len - 1; i < len / 2; i++, j--)
+		if (arr[i] != arr[j])
+			return (0);
+	return (1);
+}
+
+/**
  * is_palindrome - check linked list is palindrome
  * @head: pointer to head of list
  * Return: 1 is palindrome, else 0
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *af_last = NULL;
 	listint_t *curr = *head;
-	listint_t *last = *head;
+	int i, len, ret;
+	char *arr;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	while (1)
+
+	for (i = 0; curr; curr = curr->next, i++)
+		;
+	len = i;
+	arr = malloc(sizeof(int) * len);
+	for (i = 0, curr = *head; curr; curr = curr->next, i++)
 	{
-		while (last->next != af_last)
-			last = last->next;
-		if (curr->n != last->n)
-			return (0);
-		if (curr == last || curr->next == last)
-			return (1);
-		curr = curr->next;
-		af_last = last;
-		last = curr;
+		arr[i] = curr->n;
 	}
+	ret = check_pal(arr, len);
+	free(arr);
+	return (ret);
 }
