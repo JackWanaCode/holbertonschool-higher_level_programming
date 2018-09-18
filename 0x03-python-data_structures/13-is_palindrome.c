97ab0fd12c4e3fdee1a314159c311207cc424b23
JackWanaCode <kazucuong@yahoo.com>
2018-09-18 23:27:27 +0000

    revised linked list

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,22 +3,6 @@
 #include "lists.h"
 
 /**
- * check_pal - check is palindrome array
- * @arr: pointer to array
- * @len: length of array
- * Return: 1 is palindrome, else 0
- */
-int check_pal(char *arr, int len)
-{
-	int i, j;
-
-	for (i = 0, j = len - 1; i < len / 2; i++, j--)
-		if (arr[i] != arr[j])
-			return (0);
-	return (1);
-}
-
-/**
  * is_palindrome - check linked list is palindrome
  * @head: pointer to head of list
  * Return: 1 is palindrome, else 0
@@ -26,21 +10,47 @@ int check_pal(char *arr, int len)
 int is_palindrome(listint_t **head)
 {
 	listint_t *curr = *head;
-	int i, len, ret;
-	char *arr;
+	listint_t *curr2 = NULL;
+	listint_t *head2 = *head;
+	listint_t *next_curr2 = *head;
+	int check = 0, i = 0, len = 0;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	for (i = 0; curr; curr = curr->next, i++)
+	for (i = 0; head2->next; head2 = head2->next, i++)
 		;
 	len = i;
-	arr = malloc(sizeof(int) * len);
-	for (i = 0, curr = *head; curr; curr = curr->next, i++)
+
+	for (i = 0; i < len; i++)
 	{
-		arr[i] = curr->n;
+		if (check == 0)
+		{
+			curr2 = next_curr2;
+			next_curr2 = next_curr2->next;
+		}
+		if (i == len / 2 + 1)
+		{
+			head2 = curr2->next;
+			curr2->next = NULL;
+			check = 1;
+		}
+		else if (check == 1)
+		{
+			next_curr2 = next_curr2->next;
+			head2->next = curr2;
+			curr2 = head2;
+			head2 = next_curr2;
+		}
 	}
-	ret = check_pal(arr, len);
-	free(arr);
-	return (ret);
+	next_curr2->next = curr2;
+	while (next_curr2)
+	{
+		if (curr->n != next_curr2->n)
+			return (0);
+		curr = curr->next;
+		next_curr2 = next_curr2->next;
+	}
+	return (1);
+
 }
