8d1604abc3a04a1240cbf4a03ed76f47374f05c2
JackWanaCode <kazucuong@yahoo.com>
2018-09-19 18:54:54 +0000

    first commit

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <Python.h>
+#include <string.h>
+#include <object.h>
+
+void print_python_list_info(PyObject *p)
+{
+	unsigned long list_size = PyList_Size(p);
+	unsigned long allocated_size = PyList_GET_SIZE(p);
+	unsigned long i = 0;
+	const char *type;
+
+	printf("[*] Size of the Python List = %lu\n", list_size);
+	printf("[*] Allocated = %lu\n", allocated_size);
+	while (i < list_size)
+	{
+		type = Py_TYPE(PyList_GetItem(p, i))->tp_name;
+ 		printf("Element %lu: %s\n", i, type);
+		i++;
+	}
+}
