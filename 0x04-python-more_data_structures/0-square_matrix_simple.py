af7fb3def9a2b4753a0fcdeb23a3c38ad471c2b2
JackWanaCode <kazucuong@yahoo.com>
2018-09-20 20:31:45 +0000

    correct pep8 check

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def square_list_num(l = []):
+def square_list_num(l=[]):
     return list(map(lambda x: (x * x), l))
+
+
 def square_matrix_simple(matrix=[]):
     return list(map(square_list_num, matrix))
