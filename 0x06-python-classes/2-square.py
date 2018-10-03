5f3d4490c6ea4e825caff9b75e564c21d9d0e29c
JackWanaCode <kazucuong@yahoo.com>
2018-10-03 05:54:46 +0000

    fix error of TypeError

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,7 @@ class Square:
     def __init__(self, size=0):
         """initialize the data."""
         self.__size = size
-        if size == str(size):
+        if size != int(size):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
