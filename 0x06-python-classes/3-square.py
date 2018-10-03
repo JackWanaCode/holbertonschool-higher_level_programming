19d88e54afb6a1d8eebdc39a8a405dbe4d6801c9
JackWanaCode <kazucuong@yahoo.com>
2018-10-03 01:12:20 +0000

    firt commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    """create Square class"""
+    def __init__(self, size=0):
+        """initialize the data."""
+        self.__size = size
+        try:
+            if size < 0:
+                int("dog")
+        except TypeError:
+            raise Exception("size must be an integer")
+        except ValueError:
+            raise Exception("size must be >= 0")
+    def area(self):
+        return self.__size * self.__size
