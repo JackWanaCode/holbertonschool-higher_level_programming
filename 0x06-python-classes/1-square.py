1b202a8569099e147071d4574f41a33bdba2fe58
JackWanaCode <kazucuong@yahoo.com>
2018-10-03 00:11:38 +0000

    correct name of _Square__size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    def __init__(self, _Square_size):
-        self._Square_size = _Square_size
+    def __init__(self, _Square__size):
+        self._Square__size = _Square__size
