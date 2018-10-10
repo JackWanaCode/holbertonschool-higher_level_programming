69cef7fde147caeff590db3af9aeca0457e121d5
JackWanaCode <kazucuong@yahoo.com>
2018-10-10 14:48:25 +0000

    correct Error Printing when width data is not valid

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -24,8 +24,8 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if type(value) is not int:
-            raise TypeError("height must be an integer")
+            raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("width must be >= 0")
         else:
             self.__width = value
