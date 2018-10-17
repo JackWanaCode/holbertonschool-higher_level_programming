b595689046fdd5ece121d8a75a3db26dee458f2c
JackWanaCode <kazucuong@yahoo.com>
2018-10-17 15:45:38 +0000

    first commit

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+slit_line = []
+file_size = 0
+num_file = {}
+ct = 0
+while True:
+    ct += 1
+    line = sys.stdin.readline()
+    split_line = line.split()
+    file_size += int(split_line[-1])
+    if split_line[-2] in num_file:
+        num_file[split_line[-2]] += 1
+    else:
+        num_file[split_line[-2]] = 0
+    if ct == 10:
+        sys.stdout.write("File size: {}\n".format(file_size))
+        for k, v in num_file.items():
+            sys.stdout.write("{}: {}\n".format(k, v))
+        num_file = {}
+        file_size = 0
+        ct = 1
+        sys.stdout.flush()
