e366219fb81deef8bab539d6ee2e82cb6b945168
JackWanaCode <kazucuong@yahoo.com>
2018-10-17 05:21:08 +0000

    add return for function

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,3 +13,5 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+        else:
+            return value
