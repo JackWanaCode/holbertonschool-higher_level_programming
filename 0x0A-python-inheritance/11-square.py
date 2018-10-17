456dbc88a65c261f582798145da6142fa79aa530
JackWanaCode <kazucuong@yahoo.com>
2018-10-17 05:53:49 +0000

    inherit super class

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,40 +1,5 @@
 #!/usr/bin/python3
-
-
-class BaseGeometry:
-    """create a new class"""
-    def area(self):
-        """calculate area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validating input data"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        else:
-            return value
-
-
-class Rectangle(BaseGeometry):
-    """new sub class"""
-    def __init__(self, width, height):
-        """initialize the data"""
-        self.__width = super().integer_validator("width", width)
-        self.__height = super().integer_validator("height", height)
-
-    def __str__(self):
-        """return str function"""
-        w = str(self.__width)
-        h = str(self.__height)
-        name = str(self.__class__.__name__)
-        lis = ["["] + [name] + ["] "] + [w] + ["/"] + [h]
-        return "".join(lis)
-
-    def area(self):
-        """overwrite area function"""
-        return self.__width * self.__height
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
