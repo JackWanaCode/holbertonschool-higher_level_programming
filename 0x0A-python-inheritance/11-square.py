b917625d5a41a74922ea901aa112d2ee93ebd3ce
JackWanaCode <kazucuong@yahoo.com>
2018-10-17 06:05:40 +0000

    fix inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,4 +6,5 @@ class Square(Rectangle):
     """new sub class"""
     def __init__(self, size):
         """initialize data"""
+        self.__size = super().integer_validator("size", size)
         super().__init__(size, size)
