71cdb1ce0fe11213a051594ae38ec4445cfc1068
JackWanaCode <kazucuong@yahoo.com>
2018-10-17 18:02:07 +0000

    fix ctrC

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 import sys
+import signal
 slit_line = []
 file_size = 0
 num_file = {}
 ct = 0
 
+def signal_handler(signal, frame):
+    sys.stdout.write("File size: {}\n".format(file_size))
+    for k, v in sorted(num_file.items()):
+        sys.stdout.write("{}: {}\n".format(k, v))
+    sys.exit()
+
 for line in sys.stdin:
-    line = sys.stdin.readline()
     split_line = line.split()
     file_size += int(split_line[-1])
     if split_line[-2] in num_file:
@@ -20,3 +26,4 @@ for line in sys.stdin:
         for k, v in sorted(num_file.items()):
             sys.stdout.write("{}: {}\n".format(k, v))
         ct = 0
+    signal.signal(signal.SIGINT, signal_handler)
