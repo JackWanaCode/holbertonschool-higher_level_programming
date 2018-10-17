c2de44eadc96edd1c242c027ef5f1d64b8f4842d
JackWanaCode <kazucuong@yahoo.com>
2018-10-17 16:17:47 +0000

    print after 10 lines

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,18 +6,17 @@ num_file = {}
 ct = 0
 
 for line in sys.stdin:
-    ct += 1
     line = sys.stdin.readline()
     split_line = line.split()
     file_size += int(split_line[-1])
     if split_line[-2] in num_file:
         num_file[split_line[-2]] += 1
+        ct += 1
     else:
-        num_file[split_line[-2]] = 0
+        num_file[split_line[-2]] = 1
+        ct += 1
     if ct == 10:
         sys.stdout.write("File size: {}\n".format(file_size))
         for k, v in sorted(num_file.items()):
             sys.stdout.write("{}: {}\n".format(k, v))
-        num_file = {}
-        file_size = 0
         ct = 0
