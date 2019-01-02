d5801008402c425b30dadc3cfadd5db1d8f8367b
JackWanaCode <kazucuong@yahoo.com>
2019-01-02 20:37:46 +0000

    delete loop to get arguments

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
 """Write a script that lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa"""
-import sys
+from sys import argv
 import MySQLdb
 
-i = 0
-for el in sys.argv:
-    if i == 1:
-        u = el
-    if i == 2:
-        pw = el
-    if i == 3:
-        d = el
-    i += 1
-db = MySQLdb.connect(host="localhost", port=3306, user=u, passwd=pw, db=d)
+db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2],
+                     db=argv[3])
 cur = db.cursor()
 cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
 rows = cur.fetchall()
