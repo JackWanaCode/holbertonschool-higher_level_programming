970299de1110f3f41f0c3e243c704c5823ccd6e3
JackWanaCode <kazucuong@yahoo.com>
2019-01-03 14:09:11 +0000

    pep 8 fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,8 +4,8 @@ if __name__ == '__main__':
     from sys import argv
     import MySQLdb
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2],
-                     db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT A.id, A.name, B.name FROM cities A LEFT OUTER JOIN" +
                 " states B ON A.state_id = B.id ORDER BY A.id")
