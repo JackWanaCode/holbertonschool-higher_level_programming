f35be790cee942ce886c19db009cd25b625ed3a4
JackWanaCode <kazucuong@yahoo.com>
2019-01-03 23:39:27 +0000

    correct relationship

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -12,4 +12,4 @@ class State(Base):
     id = Column(Integer, autoincrement=True, unique=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all")
+    cities = relationship("City", cascade='all, delete', backref='state')
