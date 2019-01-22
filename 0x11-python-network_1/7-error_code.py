dd1b3993bb8346e3effb3dbb65c1d8f50e4dcb58
JackWanaCode <kazucuong@yahoo.com>
2019-01-22 00:10:53 +0000

    first commit

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    if r.status_code == requests.codes.ok:
+        print(r.text)
+    else:
+        print("Error code: {}".format(r.status_code))
