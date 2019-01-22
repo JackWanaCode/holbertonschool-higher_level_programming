e1b7fad311e53e0f7f014a6a42c10123ad9d4422
JackWanaCode <kazucuong@yahoo.com>
2019-01-22 01:32:27 +0000

    fix pep8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,14 +2,14 @@
 import requests
 from sys import argv
 if __name__ == "__main__":
-   var = ""
-   if len(argv) > 1:
-      var = argv[1]
-   r = requests.post('http://0.0.0.0:5000/search_user', data={'q': var})
-   if (var == ""):
-      print("No result")
-   else:
-      try:
-         print("[{}] {}".format(r.json()['id'], r.json()['name']))
-      except:
-         print("Not a valid JSON")
+    var = ""
+    if len(argv) > 1:
+        var = argv[1]
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': var})
+    if (var == ""):
+        print("No result")
+    else:
+        try:
+            print("[{}] {}".format(r.json()['id'], r.json()['name']))
+        except:
+            print("Not a valid JSON")
