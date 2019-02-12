75bcf527cdbc88a3f500dcf35f648998b61966b1
JackWanaCode <kazucuong@yahoo.com>
2019-02-12 21:56:46 +0000

    add My number: before the result

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@ if the first argument can be converted to an integer
 let value;
 let num;
 if ((value = process.argv[2]) && (num = parseInt(value))) {
-  console.log(num);
+  console.log('My number: ' + num);
 } else {
   console.log('Not a number');
 }
