3f03bbfc76536384031c6e4e9bae5f35472ba53b
JackWanaCode <kazucuong@yahoo.com>
2019-02-12 23:14:49 +0000

    first commit

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/* script that searches the second biggest integer in the list of arguments
+*/
+let len = process.argv.length;
+let first;
+let second;
+if (len <= 3) {
+  console.log(0);
+} else {
+  let arr = process.argv;
+  first = second = arr[2];
+  for (let i = 3; i < len; i++) {
+    if (first < arr[i]) {
+      second = first;
+      first = arr[i];
+    } else if (second < arr[i]) {
+      second = arr[i];
+    }
+  }
+  console.log(second);
+}
