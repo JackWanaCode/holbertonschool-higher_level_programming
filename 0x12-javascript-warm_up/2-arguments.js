a40dc6f66542d148795ffa1272b30b2e6bd10402
JackWanaCode <kazucuong@yahoo.com>
2019-02-12 21:17:46 +0000

    first commit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* prints a message depending of the number of arguments passed
+*/
+if (process.argv[2]) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
