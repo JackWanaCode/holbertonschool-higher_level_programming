9740a8cc91c2c951fa78f30a98bdc5c0542eea30
JackWanaCode <kazucuong@yahoo.com>
2019-02-12 23:34:44 +0000

    add flag to check the first loop

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,17 +4,19 @@
 let len = process.argv.length;
 let first;
 let second;
+let check = 1;
 if (len <= 3) {
   console.log(0);
 } else {
   let arr = process.argv;
   first = second = arr[2];
   for (let i = 3; i < len; i++) {
-    if (first < arr[i]) {
+    if (first <= arr[i]) {
       second = first;
       first = arr[i];
-    } else if (second === first || second < arr[i]) {
+    } else if (check || second < arr[i]) {
       second = arr[i];
+      check = 0;
     }
   }
   console.log(second);
