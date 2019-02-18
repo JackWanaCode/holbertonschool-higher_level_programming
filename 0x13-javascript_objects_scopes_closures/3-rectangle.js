c846e27210a8693c9854baa8637802afd07a7654
JackWanaCode <kazucuong@yahoo.com>
2019-02-18 22:01:02 +0000

    first commit

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    this._width = w;
+    this._height = h;
+  }
+  get _width () {
+    return this.width;
+  }
+  set _width (w) {
+    if (w <= 0 || w == null) {
+      return;
+    }
+    this.width = w;
+  }
+  get _height () {
+    return this.height;
+  }
+  set _height (h) {
+    if (h <= 0 || h == null) {
+      delete this.width;
+    } else if (this.width) {
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
