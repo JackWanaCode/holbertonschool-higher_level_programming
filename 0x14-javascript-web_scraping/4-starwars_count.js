4764ba02627cb76ef6204f8f17fd976ca16293bd
JackWanaCode <kazucuong@yahoo.com>
2019-02-19 22:10:16 +0000

    remove full link of peop;e 18

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
 let url = process.argv[2];
-let check = 'https://swapi.co/api/people/18/';
 let count = 0;
 request(url, function (err, response, body) {
   if (err) throw err;
   let result = JSON.parse(body).results;
   for (let i = 0; i < result.length; i++) {
-    if (result[i].characters.indexOf(check) !== -1) {
+    if (result[i].characters.join('').includes('/18/')) {
       count++;
     }
   }
