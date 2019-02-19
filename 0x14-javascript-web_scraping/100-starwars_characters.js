817d6f72cd70f5808252dd14039cd45baca030fd
JackWanaCode <kazucuong@yahoo.com>
2019-02-19 21:29:51 +0000

    add id for argument

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,7 +2,7 @@
 const request = require('request');
 let next = '';
 let url = 'http://swapi.co/api/people/';
-let check = 'https://swapi.co/api/films/3/';
+let check = 'https://swapi.co/api/films/' + process.argv[2] + '/';
 function func (n = next, u = url) {
   request.get(u + n, function (err, response, body) {
     if (err) {
