b405eb78de7106ba6bb38d8081b20574c8546606
JackWanaCode <kazucuong@yahoo.com>
2019-02-22 04:07:08 +0000

    first commit

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+$('#toggle_header').click(function () {
+  if ($('#toggle_header').hasClass('red')) {
+    $('#toggle_header').removeClass('red');
+    $('#toggle_header').addClass('green');
+  } else {
+    if ($('#toggle_header').hasClass('green')) {
+      $('#toggle_header').removeClass('green');
+    }
+    $('#toggle_header').addClass('red');
+  }
+});
